85661f86520f0e352eaad967b61a50d0e6575388
gleydson21 <gleydson.dias@souunit.com.br>
2023-06-10 04:27:20 -0300

    criando o front end da aplicação

--- src/App.js
+++ src/App.js
@@ -1,23 +1,145 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import Formulario from './Formulario';
+import Tabela from './Tabela';
 
 function App() {
+
+  //Obbjeto Produto
+  const produto ={
+    codigo: 0,
+    nome:'',
+    marca:''
+  }
+
+  //UseState
+  const [btnCadastrar, setBtnCadastrar] = useState(true);
+  const [produtos, setProdutos] = useState([]);
+  const [objProduto, setObjProduto] = useState(produto);
+
+  //UseEffect
+  useEffect(()=>{
+      fetch("http://localhost:8080/listar")
+      .then(retorno => retorno.json())
+      .then(retorno_convertido => setProdutos(retorno_convertido));
+}, [] );
+
+//Obtendo os dados do formulario
+  const aoDigitar = (e) => {
+    setObjProduto({...objProduto,[e.target.name]:e.target.value});
+  }
+
+  //Cadastrar produto
+  const cadastrar = () =>{
+    fetch('http://localhost:8080/cadastrar',{
+      method: 'POST',
+      body: JSON.stringify(objProduto),
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept':'application/json'
+      }
+    })
+    .then(retorno => retorno.json())
+    .then(retorno_convertido => {
+         if(retorno_convertido.mensagem !== undefined){
+          alert(retorno_convertido.mensagem);
+         }else{
+          setProdutos([...produtos,retorno_convertido]);
+          alert('Produto cadastrato com sucesso!');
+          limparFormulario();
+         }
+    })
+  }
+  //Alterar
+  const alterar = () =>{
+    fetch('http://localhost:8080/alterar',{
+      method: 'put',
+      body: JSON.stringify(objProduto),
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept':'application/json'
+      }
+    })
+    .then(retorno => retorno.json())
+    .then(retorno_convertido => {
+         if(retorno_convertido.mensagem !== undefined){
+          alert(retorno_convertido.mensagem);
+         }else{
+          //Mensagem
+          alert('Produto alterado com sucesso!');
+
+            //Cópia de vetor de produtos
+    let vetorTemp = [...produtos];
+
+    //Índice
+      let indice = vetorTemp.findIndex((p) =>{
+        return p.codigo === objProduto.codigo;
+      });
+
+      //Alterar produto do vetorTemp
+      vetorTemp[indice] = objProduto;
+
+      //Atualizar o vetor de produtos
+      setProdutos(vetorTemp);
+
+
+          //Limpar o formulario
+          limparFormulario();
+         }
+    })
+  }
+
+  //Remover produto
+  const remover = () =>{
+    fetch('http://localhost:8080/remover/' + objProduto.codigo,{
+      method: 'delete',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept':'application/json'
+      }
+    })
+    .then(retorno => retorno.json())
+    .then(retorno_convertido => {
+    //Mensagem
+    alert(retorno_convertido.mensagem);
+
+    //Cópia de vetor de produtos
+    let vetorTemp = [...produtos];
+
+    //Índice
+      let indice = vetorTemp.findIndex((p) =>{
+        return p.codigo === objProduto.codigo;
+      });
+
+      //Remover produto do vetorTemp
+      vetorTemp.splice(indice,1);
+
+      //Atualizar o vetor de produtos
+      setProdutos(vetorTemp);
+
+      //Limpar formulário
+      limparFormulario();
+
+    })
+  }
+
+  //Limpar formulário
+  const limparFormulario = () =>{
+    setObjProduto(produto);
+  }
+
+  // Selecionar produto
+  const selecionarProduto = (indice)=>{
+    setObjProduto(produtos[indice]);
+    setBtnCadastrar(false);
+  }
+
+  //Retorno  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+     <Formulario botao={btnCadastrar} eventoTeclado={aoDigitar} cadastrar={cadastrar} obj={objProduto} remover ={remover}  alterar={alterar} cancelar = {limparFormulario} />
+    <Tabela vetor={produtos} selecionar={selecionarProduto} />
     </div>
   );
 }
